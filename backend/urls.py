0ec6de1f9f776f3b281c2f60cdde125af924af3e
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-04-06 10:16:20 +0500

    Extending the user model

--- backend/urls.py
+++ backend/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
-from django.conf.urls.static import static
 
-from backend import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 sitemaps = {
     'posts': PostSitemap,
