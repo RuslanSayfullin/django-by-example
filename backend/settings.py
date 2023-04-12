b330787741f933f2bd5d6f8f342abf4c207cffe1
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-04-12 10:15:52 +0500

    Creating a profile for users that register with social authentication

--- backend/settings.py
+++ backend/settings.py
@@ -162,3 +162,17 @@ SOCIAL_AUTH_TWITTER_SECRET = ''     # Twitter API Secret
 # Authentication using Google
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = social_auth_google_oauth2_key         # Google Client ID
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = social_auth_google_oauth2_secret   # Google Client Secret
+
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
