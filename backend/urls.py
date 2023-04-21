ddb1b9a409706e6d57fdede42962224c62d8ad01
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-04-21 10:19:14 +0500

    Building catalog views

--- backend/urls.py
+++ backend/urls.py
@@ -12,9 +12,9 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls', namespace='shop')),
     path('blog/', include('blog.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('account/', include('account.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('images/', include('images.urls', namespace='images')),
