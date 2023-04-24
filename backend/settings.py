314884a3ce848e3f4b8eb6b1f33288a0f3c9c25e
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-04-24 14:24:03 +0500

    Создание контекстного процессора для текущей корзины.

--- backend/settings.py
+++ backend/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
 
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
