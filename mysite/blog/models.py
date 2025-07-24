fff7e0c04be14f50eab2f09597af842e01e62002
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2025-07-24 17:47:56 +0500

    First Chapter

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -17,6 +21,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    object = models.Manager()   # менеджер по умолчанию
+    published = PublishedManager()  # приклодной менеджер
 
     class Meta:
         ordering = ['-publish']
